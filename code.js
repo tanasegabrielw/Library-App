afb6cc33609c1bd75e5f3e1de70944d2623a9b4e
Gabriel <72333512+tanasegabrielw@users.noreply.github.com>
2024-04-24 07:36:11 +0300

    fix: Fix hover effect on buttons

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+// Values from books 
 const titleBook=document.querySelector('#title');
 const authorBook=document.querySelector('#author');
 const pagesBook=document.querySelector('#pages');
@@ -6,7 +7,17 @@ const submitBook=document.querySelector('.form-button');
 
 const bookShelf=document.querySelector('.book-shelf');
 const bookCard=document.querySelector('.book-card');
+// form pop-up dialog
+const dialog =document.querySelector('dialog');
+const showButton=document.querySelector('.new-book');
+const closeButton=document.querySelector('.close-form');
 
+showButton.addEventListener('click',()=>{
+    dialog.showModal();
+})
+closeButton.addEventListener('click',()=>{
+    dialog.close();
+})
 
 const myLibrary=[];
 
